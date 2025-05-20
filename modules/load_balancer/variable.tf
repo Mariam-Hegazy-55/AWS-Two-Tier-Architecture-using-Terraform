103b851d7f94d660d6f4619b374c8d953908e6e2
Mariam Mohamed Hegazy <mariamhegazy550@gmail.com>
2025-05-20 16:18:41 +0300

    Add files via upload

---
+++ modules/load_balancer/variable.tf
@@ -0,0 +1,17 @@
+/*
+variable "subnets" {
+  type = list(string)
+  description =" the subnets where the ALB will be placed."
+}
+
+variable "vpc_id" {
+    type = string
+    description = "The VPC where the target group exists."
+}
+
+variable "instances" {
+    type = list(string)
+    description = "the target instances ids"
+  
+}
+*/
